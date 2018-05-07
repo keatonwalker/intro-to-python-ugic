bff19790c9f0c62c51f5ec1578616ab10faef438
keatonwalker <keatwalker@gmail.com>
2018-05-06 21:01:10 -0600

    added more lists and dicts

--- 3-control/exercise_flow.py
+++
@@ -1,8 +0,0 @@
-# Task 0: Make 'time' plural when number is greater than one.
-number_list = [1, 2, 3]
-for number in number_list:
-    print('This print has run {} time'.format(number))
-
-# Task 1: Create a loop that will print every word of the string on a new line
-word_string = 'willy nilly billy dilly'
-# print(string, end='') will print on the same line
--- 4-strings/exercise_strings.py
+++
@@ -1,9 +0,0 @@
-# Task 0: Complete the format string with the following varibles
-noun1 = None
-verb = None
-noun2 = None
-format_string = 'The {} quickly {} to the {}'
-print(format_string)
-
-# Task 1: Use string slicing to as many words as you can from 'knowledge'
-know = 'knowledge'
